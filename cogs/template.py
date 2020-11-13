7dc1ded21c1aecb082918618986faaf1f884483f
Levi Pole <leviwp48@gmail.com>
2020-11-13 05:31:11 +0000

    Adding text replacement with variables in template object

--- cogs/template.py
+++ cogs/template.py
@@ -1,31 +1,78 @@
+import re
+
 class Template:
   # Todo: back end work to build variables into user input 
   # such that: ".new "person" "tom"" "person is good" -> in back end it would look like this : "person_1 is good". 
   # this way the template static is built once new template is called and variables are built in
+
   def __init__(self):
+    self.name = ""
     self.keywords_list = []
+    self.keyword_map = {}
     self.text = ""
 
-  def new_template(self, ctx, keywords=[], keywords_amount=[], text=""):
-    
-    if(len(keywords) != len(keywords_amount)):
-      return
-    
-    count = 0
+  def new_template(self, ctx, name, keywords=[], text=""):
+    self.name = name
+    '''
+    Actually I think that I have a better idea. Instead I'm going to make the text based on what the user provides as arguments. 
+    Ex:
+    Roster:
+    - [player]
+    - [player]
+    - [player]
 
-    while(count < len(keywords)): 
-      self.keywords_list[count] = 
+    each player argument that the user gives I'll put it into a queue and pop them out in the same order they went input
 
-      print("adding to dict: " + keywords[count] + "  : " + keywords_amount[count])
+    In this way I won't need the amount of keywords
+
+    Okay so I'm going to use Regex to filter out these wordsth
+    the patter that I'm looking for is = '\[\w\]'
+    '''    
+
+
+    # okay coming back to this let me figure out what ths does
+    # this finds the matches of my desired word and the # of occurense, it then prints the matches and the count. 
+  
+    for key in keywords:
+      count = 0
+      matches = re.finditer(rf"\[({key})\]", text)
+      if matches:
+        for match in matches:
+          count = count + 1
+        self.keyword_map[key] = count
+      else:
+        print("no match")
       
-      count += 1
-    print(self.keywords_list)
+    for key in self.keyword_map:
+      temp_count = self.keyword_map[key]
+      while temp_count != 0:
+        text = text.replace("[" + key + "]", "{" + key + "}", 1)
+        temp_count -= 1
+        #print(text)
     self.text = text
+    #print(text.format(person="lucas"))
 
+
+    #for match in matches:
+     # print("finding matches: " + str(match)) 
+
+
+    self.keywords_list = keywords 
+    #print("keywords given: " + self.keywords_list)
+    
+    #self.text = text.split(' ')
+    #print(self.text)
+    
+   
   def show(self):
     return self.text
+  
+  def use(keywords_args=[]):
+    print("hi")
+    
     
 
     
     
     
+ 
--- cogs/template_saver.py
+++ cogs/template_saver.py
@@ -9,37 +9,32 @@ class TemplateSaver(commands.Cog, name='Template Saver'):
   def __init__(self, bot):
     self.bot = bot
     
-
-  @commands.command(name="new_template")
-  async def new_template(self, ctx, keywords, keywords_amount, message):
+  @commands.command(name="new")
+  async def new(self, ctx, name, keywords, message):
     
     keywords_list = []
     for key in keywords.split(', '):
       keywords_list.append(key)
-    keywords_amount_list = []
-    for amount in keywords_amount.split(', '):
-      keywords_amount_list.append(amount)
 
-    print("keywords: " + str(keywords_list))
-    print("keywords_amount: " + str(keywords_amount_list))
-    print("message: " + message)
+    #print("keywords: " + str(keywords_list))
+    #print("message: " + message)
 
     new_template = Template()
-    new_template.new_template(ctx, keywords_list, keywords_amount_list, message)
+    new_template.new_template(ctx, name,keywords_list, message)
 
-    await ctx.send(new_template.show())
+    await ctx.send(new_template.show)
 
     self.saved_templates.append(new_template)
 
-  @commands.command(name="view_templates")
-  async def view_templates(self, ctx):
+  @commands.command(name="view")
+  async def view(self, ctx):
     template_count = 1
-    template_list = ""
-    for i in self.saved_templates:
-      template_list = template_list + "[" + str(template_count) + "] " + i + "\n"
-      template_count+=1
-
-    await ctx.send(f"Saved templates: \n================ \n{template_list}")
+    
+    #for i in self.saved_templates:
+      #template_list = template_list + "[" + str(template_count) + "] " + i + "\n"
+      #template_count+=1
+    print(self.saved_templates)
+    await ctx.send(f"Saved templates: \n================ \n {template_count}: {self.saved_templates[0].name}")
 
   @commands.command(name="use_template")
   async def use_template(self,ctx):
